088a1c2aface79ad508ed1437da8874490192705
Kinugasa Hirata <kinugasa.hirata@gmail.com>
2026-02-20 11:42:53 +0900

    Fix: add scroll-area UI component

---
+++ components/ui/scroll-area.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from 'react'
+
+interface ScrollAreaProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode
+}
+
+export function ScrollArea({ children, className = '', ...props }: ScrollAreaProps) {
+  return (
+    <div className={overflow-auto } {...props}>
+      {children}
+    </div>
+  )
+}
